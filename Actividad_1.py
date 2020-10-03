0e9299384ee0592b3e37322ad85a81ca2f45f1b6
Ashleysa <ashleysa2001@gmail.com>
2020-10-03 11:29:17 -0400

    Primera actividad con los 4 retos

---
+++ Actividad_1.py
@@ -0,0 +1,28 @@
+# Reto 1
+print('Reto 1:')
+print()
+print("The ship's name is 'Brave'")
+print()
+print("Cat's paws.")
+print()
+print('The word "Ciao" means "Hello" in Italian')
+print()
+print('And "Ciao" is Italian "Bye"')
+
+# Reto 2
+print()
+print('Reto 2:')
+print()
+print('Mi nombre es Ashley Solorzano')
+
+# Reto 3
+print()
+print('Reto 3:')
+print()
+print('Nosotros aprendemos Python!')
+
+# Reto 4
+print()
+print('Reto 4:')
+print()
+print('2 + 2 = 4')
