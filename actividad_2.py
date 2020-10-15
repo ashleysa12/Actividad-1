401e965b4dca805469598f3f6d6e4195e27874b3
Ashleysa <ashleysa2001@gmail.com>
2020-10-14 23:43:02 -0400

    Actividad_2.0

---
+++ actividad_2.py
@@ -0,0 +1,99 @@
+# Primer reto
+print("Reto #1:")
+print("primero")
+print()
+print("segundo")
+print()
+print("tercero")
+print()
+
+# Segundo reto
+print()
+print("Reto #2:")
+print("Necesitamos")
+print()
+print()
+print("Aprender Python")
+print()
+print()
+print("Es rápido, es posible")
+print()
+
+# Tercer reto
+print()
+print("Reto #3:")
+print("****")
+print()
+print("*  *")
+print()
+print("*  *")
+print()
+print("****")
+
+# Cuarto reto
+print()
+print("Reto #4:")
+print()
+print("Monday")
+print("Tuesday Wednesday")
+print()
+print("Thursday")
+print("Este código imprime 5 líneas")
+print()
+
+# Quinto reto
+print()
+print("Reto #5:")
+print("O X X")
+print()
+print("O X O")
+print()
+print("X O X")
+print()
+
+# Sexto reto
+print()
+print("Reto #6:")
+print('Always write beautiful code!')
+print()
+
+# Septimo reto
+print()
+print("Reto #7:")
+print('Those in favor, say "aye"')
+print("Wow! It's amazing")
+print()
+
+# Octavo reto
+print()
+print("Reto #8:")
+print("""' '' '''
+
+' '' '''
+
+' '' '''""") 
+print() 
+
+# Noveno reto
+print()
+print("Reto #9:")
+print("""'
+
+'"'
+
+'"'"'
+
+'"'"'"'""")
+print()
+
+# Decimo reto
+print()
+print("Reto #10:")
+print("Did that stop the old Grinch?")
+print()
+print("No! The Grinch simply said,")
+print()
+print('"If can\'t find a reindeer,')
+print()
+print('I\'ll make one instead!"')
+print()
